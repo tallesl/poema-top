f0ac83b81484f310668e04bcc15ae9f21d298f1f
Talles L <talleslasmar@gmail.com>
2024-09-14 16:05:39 -0300

    Movida parte da lógica de one hot para a classe de vocabulário

--- poema_top/pre_processamento/janelas_one_hot.py
+++ poema_top/pre_processamento/janelas_one_hot.py
@@ -8,32 +8,43 @@ class JanelasOneHot:
         assert texto_completo
         assert vocabulario
 
-        janelas_menos_ultimo, janelas_ultimo = _janelas(texto_completo)
-        self.x, self.y = _one_hot(janelas_menos_ultimo, janelas_ultimo, vocabulario)
+        janelas_menos_ultimo, janelas_ultimo = _quebra_em_janelas(texto_completo)
+        self.x, self.y = _one_hot_janelas(janelas_menos_ultimo, janelas_ultimo, vocabulario)
 
-def _janelas(texto_completo: str) -> tuple[list[str], list[str]]:
+def _quebra_em_janelas(texto_completo: str) -> tuple[list[str], list[str]]:
     assert texto_completo
+
+    total_caracteres = len(texto_completo)
 
     janelas_menos_ultimo = [] # janelas de texto (x) sem o último caractere (y)
     janelas_ultimo = [] # último caractere das janelas (y)
 
-    # índice do primeiro caractere da última janela (começo da última janela)
-    indice_comeco_ultima_janela = len(texto_completo) - configuracao.tamanho_janela
-
     # iterando do primeiro caractere da primeira janela (zero) até o primeiro caractere da última janela
     # a cada iteração pulamos a distância configurada
-    for i in range(0, indice_comeco_ultima_janela, configuracao.distancia_janela):
+    for i in range(0, total_caracteres, configuracao.distancia_janela):
 
-        exceto_ultimo = texto_completo[i:i + configuracao.tamanho_janela] # window characters minus last one
-        ultimo = texto_completo[i + configuracao.tamanho_janela] # window last character
+        inicio_janela = i
+        fim_janela = i + configuracao.tamanho_janela - 1
+
+        # a última janela é descartada para garantir que as janelas tenham o mesmo tamanho
+        if fim_janela >= total_caracteres:
+            break
+
+        exceto_ultimo = texto_completo[inicio_janela:fim_janela] # janelas de caracteres menos o último
+        ultimo = texto_completo[fim_janela] # último caractere
+
+        # exceto último + último = janela completa 
+        assert len(exceto_ultimo) + len(ultimo) == configuracao.tamanho_janela
 
         janelas_menos_ultimo.append(exceto_ultimo)
         janelas_ultimo.append(ultimo)
 
-    # windows and windows_last have matching indexes and the same length
+    # janelas_menos_ultimo e janelas_ultimo devem possuir índices que casam e devem ter o mesmo tamanho
+    assert len(janelas_menos_ultimo) == len(janelas_ultimo)
+
     return janelas_menos_ultimo, janelas_ultimo
 
-def _one_hot(janelas_menos_ultimo: list[str], janelas_ultimo: list[str], vocabulario: Vocabulario) -> tuple[
+def _one_hot_janelas(janelas_menos_ultimo: list[str], janelas_ultimo: list[str], vocabulario: Vocabulario) -> tuple[
     np.ndarray, np.ndarray]:
 
     assert janelas_menos_ultimo
@@ -43,31 +54,25 @@ def _one_hot(janelas_menos_ultimo: list[str], janelas_ultimo: list[str], vocabul
 
     total_janelas = len(janelas_menos_ultimo)
 
-    x = np.zeros((total_janelas, configuracao.tamanho_janela, vocabulario.tamanho), dtype=bool)
-    y = np.zeros((total_janelas, vocabulario.tamanho), dtype=bool)
+    # "x" são as janelas sem o último caracteres codificadas em one hot
+    # shape: (total janelas, quantidade de caracteres da janela - 1, tamanho do vocabulario)
+
+    # "y" são os últimos caracteres (de cada janela) codificados em one hot
+    # shape: (total janelas, tamanho do vocabulário)
+
+    shape_x = (total_janelas, configuracao.tamanho_janela - 1, vocabulario.tamanho)
+    shape_y = (total_janelas, vocabulario.tamanho)
+
+    x = np.zeros(shape_x, dtype=bool)
+    y = np.zeros(shape_y, dtype=bool)
 
     # para cada janela
-    for i, janela_x in enumerate(janelas_menos_ultimo):
+    for i in range(total_janelas):
 
-        # para cada caractere na janela
-        for j, caractere_x in enumerate(janela_x):
-
-            # obtendo índice do caractere (x)
-            k = vocabulario.obtem_indice[caractere_x]
-
-            # "i" indica onde nas janelas, "j" indica onde na janela atual, "k" qual o índice do caractere atual
-            x[i, j, k] = 1
-
-        # obtendo índice do caractere (y)
+        janela_x = janelas_menos_ultimo[i]
         caractere_y = janelas_ultimo[i]
-        j = vocabulario.obtem_indice[caractere_y]
 
-        # "i" indica onde nos últimos caracters, "j" qual o índice do caractere atual
-        y[i, j] = 1
+        x[i] = vocabulario.one_hot_texto(janela_x)
+        y[i] = vocabulario.one_hot_caractere(caractere_y)
 
-    # "x" são as janelas sem o último caracteres codificadas em one hot
-    # shape: (total janelas, quantidade de caracteres da janela, tamanho do vocabulario)
-
-    # "y" são os últimos caracteres codificados em one hot
-    # shape: (total janelas, tamanho do vocabulário)
     return x, y
--- poema_top/pre_processamento/vocabulario.py
+++ poema_top/pre_processamento/vocabulario.py
@@ -1,6 +1,11 @@
+import numpy as np
+
+from ..configuracao import tamanho_janela
+
 class Vocabulario:
     def __init__(self, texto_completo: str):
         assert texto_completo
+        assert len(texto_completo) > tamanho_janela
 
         # caracteres únicos ordenados
         caracteres = sorted(set(texto_completo))
@@ -9,3 +14,35 @@ class Vocabulario:
         self.obtem_indice = {c: i for i, c in enumerate(caracteres)}
         self.obtem_caractere = {i: c for i, c in enumerate(caracteres)}
         self.tamanho = len(caracteres)
+
+
+    def one_hot_texto(self, texto: str) -> np.ndarray[np.ndarray[bool]]:
+        assert texto
+
+        # convertendo cada caractere do texto para one hot
+        array = [self.one_hot_caractere(c) for c in texto]
+
+        # convertendo para um array do NumPy
+        array = np.array(array)
+
+        assert array.shape == (len(texto), self.tamanho)
+        assert array.dtype == bool
+
+        return array
+
+
+    def one_hot_caractere(self, caractere: str) -> np.ndarray[bool]:
+        assert caractere
+        assert len(caractere) == 1
+
+        # criando um array de valores boleanos do tamanho do vocabulário
+        # os índices deste array casam com os índices do vocabulário (mesmo caractere no mesmo índice)
+        array = np.zeros(self.tamanho, dtype=bool)
+
+        # obtendo o índice do caractere passado
+        indice = self.obtem_indice[caractere]
+
+        # trocando de 0 para 1 o índice do caractere passado, no array que foi inicializado com zeros
+        array[indice] = 1
+
+        return array
