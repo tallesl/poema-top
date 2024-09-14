48632ddfb69edc86a0f56230f8c06136985916b1
Talles L <talleslasmar@gmail.com>
2024-09-14 17:56:41 -0300

    Trocado termo menos para exceto

--- poema_top/pre_processamento/janelas_one_hot.py
+++ poema_top/pre_processamento/janelas_one_hot.py
@@ -8,15 +8,15 @@ class JanelasOneHot:
         assert texto_completo
         assert vocabulario
 
-        janelas_menos_ultimo, janelas_ultimo = _quebra_em_janelas(texto_completo)
-        self.x, self.y = _one_hot_janelas(janelas_menos_ultimo, janelas_ultimo, vocabulario)
+        janelas_exceto_ultimo, janelas_ultimo = _quebra_em_janelas(texto_completo)
+        self.x, self.y = _one_hot_janelas(janelas_exceto_ultimo, janelas_ultimo, vocabulario)
 
 def _quebra_em_janelas(texto_completo: str) -> tuple[list[str], list[str]]:
     assert texto_completo
 
     total_caracteres = len(texto_completo)
 
-    janelas_menos_ultimo = [] # janelas de texto (x) sem o último caractere (y)
+    janelas_exceto_ultimo = [] # janelas de texto (x) sem o último caractere (y)
     janelas_ultimo = [] # último caractere das janelas (y)
 
     # iterando do primeiro caractere da primeira janela (zero) até o primeiro caractere da última janela
@@ -30,29 +30,29 @@ def _quebra_em_janelas(texto_completo: str) -> tuple[list[str], list[str]]:
         if fim_janela >= total_caracteres:
             break
 
-        exceto_ultimo = texto_completo[inicio_janela:fim_janela] # janelas de caracteres menos o último
+        exceto_ultimo = texto_completo[inicio_janela:fim_janela] # janelas de caracteres exceto o último
         ultimo = texto_completo[fim_janela] # último caractere
 
         # exceto último + último = janela completa 
         assert len(exceto_ultimo) + len(ultimo) == configuracao.tamanho_janela
 
-        janelas_menos_ultimo.append(exceto_ultimo)
+        janelas_exceto_ultimo.append(exceto_ultimo)
         janelas_ultimo.append(ultimo)
 
-    # janelas_menos_ultimo e janelas_ultimo devem possuir índices que casam e devem ter o mesmo tamanho
-    assert len(janelas_menos_ultimo) == len(janelas_ultimo)
+    # janelas_exceto_ultimo e janelas_ultimo devem possuir índices que casam e devem ter o mesmo tamanho
+    assert len(janelas_exceto_ultimo) == len(janelas_ultimo)
 
-    return janelas_menos_ultimo, janelas_ultimo
+    return janelas_exceto_ultimo, janelas_ultimo
 
-def _one_hot_janelas(janelas_menos_ultimo: list[str], janelas_ultimo: list[str], vocabulario: Vocabulario) -> tuple[
+def _one_hot_janelas(janelas_exceto_ultimo: list[str], janelas_ultimo: list[str], vocabulario: Vocabulario) -> tuple[
     np.ndarray, np.ndarray]:
 
-    assert janelas_menos_ultimo
+    assert janelas_exceto_ultimo
     assert janelas_ultimo
     assert vocabulario
-    assert len(janelas_menos_ultimo) == len(janelas_ultimo)
+    assert len(janelas_exceto_ultimo) == len(janelas_ultimo)
 
-    total_janelas = len(janelas_menos_ultimo)
+    total_janelas = len(janelas_exceto_ultimo)
 
     # "x" são as janelas sem o último caracteres codificadas em one hot
     # shape: (total janelas, quantidade de caracteres da janela - 1, tamanho do vocabulario)
@@ -69,7 +69,7 @@ def _one_hot_janelas(janelas_menos_ultimo: list[str], janelas_ultimo: list[str],
     # para cada janela
     for i in range(total_janelas):
 
-        janela_x = janelas_menos_ultimo[i]
+        janela_x = janelas_exceto_ultimo[i]
         caractere_y = janelas_ultimo[i]
 
         x[i] = vocabulario.one_hot_texto(janela_x)
