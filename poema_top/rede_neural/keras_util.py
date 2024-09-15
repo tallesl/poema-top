a28340bf04c727b6ce9b0970c422250cb8e39028
Talles L <talleslasmar@gmail.com>
2024-09-15 19:46:07 -0300

    Adicionado método para obter VRAM consumida

--- poema_top/rede_neural/keras_util.py
+++ poema_top/rede_neural/keras_util.py
@@ -15,6 +15,7 @@ def alocar_memoria_aos_poucos():
     for gpu in gpus:
       tf.config.experimental.set_memory_growth(gpu, True)
 
+
 class CallbackFimEpoca(Callback):
     def __init__(self, callback: Callable[[], None], pular_epocas: Optional[int] = None):
         self.callback = callback
@@ -26,3 +27,18 @@ class CallbackFimEpoca(Callback):
             return
 
         self.callback()
+
+
+def obtem_vram() -> tuple[int, int]:
+    import tensorflow as tf
+
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    gpu = gpus[0] # utilizando apenas a primeira GPU
+
+    device = gpu.name.lstrip('/physical_device:')
+    memory_info = tf.config.experimental.get_memory_info(device)
+
+    vram_atual_mb = memory_info['current'] / 1024**2
+    vram_pico_mb = memory_info['peak'] / 1024**2
+
+    return vram_atual_mb, vram_pico_mb
