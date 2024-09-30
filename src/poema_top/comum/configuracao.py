1a52a381723f0bee4906937abdd11b6dd5925620
Talles L <talleslasmar@gmail.com>
2024-09-30 16:30:41 -0300

    Renomeado diretório

--- src/poema_top/comum/configuracao.py
+++ src/poema_top/comum/configuracao.py
@@ -1,5 +1,5 @@
 # caminho do arquivo com texto a ser utilizado no treino
-dataset_txt = '../dataset_treino/poemas-10k-llama3.1.txt'
+dataset_txt = '../dataset/poemas-10k-llama3.1.txt'
 
 # convertendo para lowercase faz o vocabulário ficar menor e diminui consideravelmente a memória ocupada
 converte_lowercase = True
